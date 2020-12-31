733a8ed168278f1afb8d6c4836e682b0c27f6d91
Dhananjayan P N <pndjay@gmail.com>
2020-12-31 13:13:51 +0530

    routing setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Layout />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Layout />
+      </div>
+    </BrowserRouter>
   );
 };
 
