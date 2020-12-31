f82c99e455d32308cf062990b1fd5f1f5e1c69a3
Dhananjayan P N <pndjay@gmail.com>
2020-12-31 15:16:08 +0530

    Routing setup

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
 import Navbar from "../Navbar/Navbar";
 import "./Layout.css";
@@ -7,9 +7,12 @@ const Layout = () => {
   return (
     <div className="Layout">
       <Route path="/" component={Navbar} />
-      <Route path="/" exact component={BurgerBuilder} />
-      <Route path="/orders" render={() => <h1>Orders Page</h1>} />
-      <Route path="/checkout" render={() => <h1>Checkout Page</h1>} />
+      <Switch>
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/orders" render={() => <h1>Orders Page</h1>} />
+        <Route path="/checkout" render={() => <h1>Checkout Page</h1>} />
+        <Route render={() => <h1>404 Page Not Found!</h1>} />
+      </Switch>
     </div>
   );
 };
