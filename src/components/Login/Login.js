206fe175be904d5902e0d409eb36a1122494dfaf
Dhananjayan P N <pndjay@gmail.com>
2021-01-04 13:41:10 +0530

    login in sing up implementation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,61 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { TextField, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const Login = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [failure, setFailure] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("Somthing went wrong! Try again later");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const logInEndpoint = "accounts:signInWithPassword?key=AIzaSyDj7fGfYjQQDBvZkybj2h2MDY4AMP5BBlI";
+
+  const handleClose = () => {
+    setLoading(false);
+    setFailure(false);
+    setErrorMessage("Somthing went wrong! Try again later");
+  };
+
+  const LoginRequest = () => {
+    setLoading(true);
+    fetch("https://identitytoolkit.googleapis.com/v1/" + logInEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        returnSecureToken: true
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.error !== undefined) {
+          if (data.error.message === "EMAIL_NOT_FOUND") {
+            setErrorMessage("The provided email is not registered! Sign up or use a registered email.");
+          }
+          if (data.error.message === "INVALID_PASSWORD") {
+            setErrorMessage("The provided password is incorrect! Enter the right password for this email.");
+          }
+          if (data.error.message === "INVALID_EMAIL") {
+            setErrorMessage("The provided email is invalid! Enter a valid email to log in.");
+          }
+          setLoading(false);
+          setEmail("");
+          setPassword("");
+          setFailure(true);
+        } else {
+          setLoading(false);
+          setEmail("");
+          setPassword("");
+          Cookies.set("user", data);
+          props.history.push("/");
+        }
+      });
+  };
 
   return (
     <div className="Login">
@@ -34,7 +85,7 @@ const Login = (props) => {
         label="Password"
       />
       <Button
-        onClick={() => {}}
+        onClick={LoginRequest}
         style={{ marginTop: 50, marginRight: "auto", marginLeft: "auto", display: "block" }}
         id="checkout-button"
         variant="outlined"
@@ -42,6 +93,26 @@ const Login = (props) => {
       >
         Log IN
       </Button>
+      <Dialog open={loading}>
+        <CircularProgress style={{ padding: "50px" }} color="secondary" />
+      </Dialog>
+      <Dialog open={failure} onClose={handleClose}>
+        <DialogTitle id="responsive-dialog-title">Error</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{errorMessage}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={() => {
+              handleClose();
+            }}
+            color="secondary"
+          >
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,97 @@
 import React, { useState } from "react";
 import "./SignUp.css";
 import { TextField, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const SignUp = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("Somthing went wrong! Try again later");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confPassword, setConfPassword] = useState("");
+  const signUpEndpoint = "accounts:signUp?key=AIzaSyDj7fGfYjQQDBvZkybj2h2MDY4AMP5BBlI";
+  const updateAccEndpoint = "accounts:update?key=AIzaSyDj7fGfYjQQDBvZkybj2h2MDY4AMP5BBlI";
+
+  const handleClose = () => {
+    setLoading(false);
+    setSuccess(false);
+    setFailure(false);
+    setErrorMessage("Somthing went wrong! Try again later");
+  };
+
+  const SignUpRequest = () => {
+    setLoading(true);
+    fetch("https://identitytoolkit.googleapis.com/v1/" + signUpEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        returnSecureToken: true
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error !== undefined) {
+          if (data.error.message === "EMAIL_EXISTS") {
+            setErrorMessage("The provided email is already registered! Log in or use a different email.");
+          }
+          setLoading(false);
+          setName("");
+          setEmail("");
+          setPassword("");
+          setConfPassword("");
+          setFailure(true);
+        } else {
+          fetch("https://identitytoolkit.googleapis.com/v1/" + updateAccEndpoint, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ idToken: data.idToken, displayName: name, returnSecureToken: true })
+          })
+            .then((resp) => {
+              if (resp.ok) {
+                setLoading(false);
+                setName("");
+                setEmail("");
+                setPassword("");
+                setConfPassword("");
+                setSuccess(true);
+              } else {
+                setLoading(false);
+                setName("");
+                setEmail("");
+                setPassword("");
+                setConfPassword("");
+                setFailure(true);
+              }
+            })
+            .catch((err) => {
+              setLoading(false);
+              setName("");
+              setEmail("");
+              setPassword("");
+              setConfPassword("");
+              setFailure(true);
+            });
+        }
+      })
+      .catch((err) => {
+        setLoading(false);
+        setName("");
+        setEmail("");
+        setPassword("");
+        setConfPassword("");
+        setFailure(true);
+      });
+  };
+
   return (
     <div className="SignUp">
       <h2 id="chk-heading">Sign Up</h2>
@@ -42,6 +127,7 @@ const SignUp = (props) => {
           setPassword(e.target.value);
         }}
         color="secondary"
+        helperText={password.length < 6 && password !== "" ? "Password too short" : ""}
         required
         style={{ width: "75%", display: "block", marginTop: 25, marginRight: "auto", marginLeft: "auto" }}
         label="Password"
@@ -53,15 +139,15 @@ const SignUp = (props) => {
         onChange={(e) => {
           setConfPassword(e.target.value);
         }}
+        helperText={confPassword !== "" && confPassword !== password ? "Passwords don't match" : ""}
         color="secondary"
         required
         style={{ width: "75%", display: "block", marginTop: 25, marginRight: "auto", marginLeft: "auto" }}
         label="Confirm Password"
       />
       <Button
-        onClick={() => {
-          console.log(name, email, password);
-        }}
+        onClick={SignUpRequest}
+        disabled={email === "" || password === "" || confPassword === "" || password !== confPassword || password.length < 6}
         style={{ marginTop: 50, marginRight: "auto", marginLeft: "auto", display: "block" }}
         id="checkout-button"
         variant="outlined"
@@ -69,6 +155,44 @@ const SignUp = (props) => {
       >
         Sign UP
       </Button>
+      <Dialog open={loading}>
+        <CircularProgress style={{ padding: "50px" }} color="secondary" />
+      </Dialog>
+      <Dialog open={success} onClose={handleClose}>
+        <DialogTitle id="responsive-dialog-title">Account Created</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Your account was successfully created! Log In!</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={() => {
+              handleClose();
+              props.history.push("/login");
+            }}
+            color="secondary"
+          >
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={failure} onClose={handleClose}>
+        <DialogTitle id="responsive-dialog-title">Error</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{errorMessage}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={() => {
+              handleClose();
+            }}
+            color="secondary"
+          >
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
